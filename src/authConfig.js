941e951af1bd0113e36f24008983134f779828ac
laurentgohebel-blip <laurentgohebel@gmail.com>
2026-05-31 17:06:41 +0200

    Update authConfig.js

--- src/authConfig.js
+++ src/authConfig.js
@@ -1,7 +1,7 @@
 // src/authConfig.js
 export const msalConfig = {
   auth: {
-    clientId: "0cc9877f-07e3-46c0-93e9-1c2f55ade914",
+    clientId: "c7631e50-4129-4e08-b27d-3445b5c37082",
     authority: "https://login.microsoftonline.com/b9cfc83f-9274-459b-a7ea-62dca3451e8c",
     redirectUri: import.meta.env.VITE_REDIRECT_URI || "https://ashy-pebble-0206b8703.7.azurestaticapps.net/",
     postLogoutRedirectUri: "/",
